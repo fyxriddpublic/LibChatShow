05bd5cff3d7296cdd8e125716fff1a4769ac4a36
fyxridd <fyxridd@163.com>
2016-05-11 08:58:18 +0800

    更新

---
+++ src/main/java/com/fyxridd/lib/show/chat/api/fancymessage/Conditional.java
@@ -0,0 +1,12 @@
+package com.fyxridd.lib.show.chat.api.fancymessage;
+
+/**
+ * 可条件选择显示的
+ * (可被FancyMessagePart的子类实现)
+ */
+public interface Conditional {
+    /**
+     * 条件显示检测
+     */
+    boolean checkCondition();
+}
