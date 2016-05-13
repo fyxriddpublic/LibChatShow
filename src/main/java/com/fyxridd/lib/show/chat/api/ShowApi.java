24e1aaa7fbb25ff66834112ae3d2eb3b4041b807
fyxridd <fyxridd@163.com>
2016-05-13 13:48:30 +0800

    更新

---
+++ src/main/java/com/fyxridd/lib/show/chat/api/ShowApi.java
@@ -0,0 +1,16 @@
+package com.fyxridd.lib.show.chat.api;
+
+import com.fyxridd.lib.core.api.fancymessage.FancyMessage;
+import com.fyxridd.lib.show.chat.ShowPlugin;
+import org.bukkit.configuration.ConfigurationSection;
+
+public class ShowApi {
+    /**
+     * @param msg 文字内容
+     * @param config 可为null
+     * @return 不为null
+     */
+    public static FancyMessage load(String msg, ConfigurationSection config) {
+        return ShowPlugin.instance.getShowManager().load(msg, config);
+    }
+}
